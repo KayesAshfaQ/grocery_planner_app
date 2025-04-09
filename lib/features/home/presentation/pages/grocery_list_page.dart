f6ec78679584ab2e700f2023367f5833792b3a21
kayesAshfaq <asfak7074@gmail.com>
2025-04-09 13:24:00 +0600

    feat: Convert GroceryListPage to StatefulWidget with TabBar for navigation

--- lib/features/home/presentation/pages/grocery_list_page.dart
+++ lib/features/home/presentation/pages/grocery_list_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-class GroceryListPage extends StatelessWidget {
+class GroceryListPage extends StatefulWidget {
   const GroceryListPage({super.key});
 
   @override
+  State<GroceryListPage> createState() => _GroceryListPageState();
+}
+
+class _GroceryListPageState extends State<GroceryListPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Grocery List Page'),
+    return Scaffold(
+      body: Column(
+        children: [
+          TabBar(
+            controller: _tabController,
+            // labelColor: Theme.of(context).colorScheme.primary,
+            tabs: const [
+              Tab(text: 'To Buy'),
+              Tab(text: 'Purchased'),
+            ],
+            onTap: (index) {
+              //TODO: Handle tab change
+            },
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                Center(child: Text('To Buy')),
+                Center(child: Text('Purchased')),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
