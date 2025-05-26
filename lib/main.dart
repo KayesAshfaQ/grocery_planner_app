7520ea6dbb3b9ccecab601932195ee44f44f07ae
KayesAshfaq <asfak7074@gmail.com>
2025-05-26 22:43:16 +0600

    feat: Add landscape orientation support in main application

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
   ]);
 
   // Initialize database
